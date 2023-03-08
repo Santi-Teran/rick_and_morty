f1f0a871a337099113fbaf9c368304af70d81972
Santi-Teran <santiagoteranmatias@gmail.com>
2023-03-08 17:02:43 -0300

    pruebas responsive

--- front/src/App.js
+++ front/src/App.js
@@ -18,7 +18,7 @@ function App () {
   const navigate = useNavigate();
 
   const onSearch = (character) => {
-    fetch(`http://localhost:3001/rickandmorty/${character.name}`)
+    fetch(`http://localhost:3001/rickandmorty/character/${character}`)
        .then((response) => response.json())
        .then((data) => {
           if (data.name) {
